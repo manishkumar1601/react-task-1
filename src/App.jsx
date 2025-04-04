19ad7b704cdedeec757f603809ef2ac7bf30461c
Vimal Jain <wo.vimal568@gmail.com>
2025-04-04 18:56:01 +0530

    use ref code initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import './App.css'
 
 function App() {
-	const [count, setCount] = useState(0);
+	const counterRef = useRef(0);
+	const inputRef = useRef(0);
+
+	function updateInputValue() {
+		if (inputRef) {
+			inputRef.current.value = counterRef.current;
+		}
+	}
 
 	function handleDecrement() {
-		setCount(prevState => prevState - 1);
+		counterRef.current = (+counterRef.current) - 1;
+		updateInputValue();
 	}
 
 	function handleIncrement() {
-		setCount(prevState => prevState + 1);
+		counterRef.current = (+counterRef.current) + 1;
+		updateInputValue();
 	}
 
 	function handleCounterInputChange(event) {
-		setCount(+(event?.target?.value));
+		counterRef.current = +(event?.target?.value);
+		updateInputValue();
 	}
 
 	return (
 		<div className='parent-wrapper'>
 			<div className="child-wrapper">
 				<div className="counter">
-					<span>Counter: {count}</span>
+					<span>Counter: {counterRef?.current}</span>
 				</div>
 				<div className="counter-form">
 					<div className="row-counter">
 						<span>Counter</span>
 						<button style={{ width: '25px', height: '25px' }} onClick={handleDecrement}>-</button>
-						<input type="number" value={count} onChange={handleCounterInputChange} />
+						<input type="number" ref={inputRef} onChange={handleCounterInputChange} />
 						<button style={{ width: '25px', height: '25px' }} onClick={handleIncrement}>+</button>
 					</div>
 				</div>
